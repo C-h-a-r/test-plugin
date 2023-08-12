fcd312240e330ecd62f0f212c93fbe4a639d122d
Char <68965006+C-h-a-r@users.noreply.github.com>
2023-08-12 17:35:03 +0100

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { CommandClient, Command, CommandContext } from "https://raw.githubusercontent.com/harmonyland/harmony/main/mod.ts";
-import { DiscordPlugin } from 'https://raw.githubusercontent.com/C-h-a-r/plugin-class/main/Plugin.ts';
+import { DiscordPlugin } from 'https://raw.githubusercontent.com/Customs-Dev-Team/plugin-class/main/Plugin.ts?token=GHSAT0AAAAAACE6FHWCYYEBLEAHKAL3PYNEZGXWTVQ';
 
 class TestPlugin extends DiscordPlugin {
     constructor(client: CommandClient) {
